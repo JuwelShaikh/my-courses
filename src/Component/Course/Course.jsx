c89c7e0e177c5d7fd2818b8d175fd08df99286dc
JuwelShaikh <juwelshaikh04@gmail.com>
2024-07-10 16:06:58 +0600

    Course compo on process.

---
+++ src/Component/Course/Course.jsx
@@ -0,0 +1,14 @@
+
+
+const Course = ({ course }) => {
+
+    const { course_name, course_details, price, cover, time } = course;
+
+    return (
+        <div className=''>
+
+        </div>
+    );
+};
+
+export default Course;
--- src/Component/Courses/Courses.jsx
+++ src/Component/Courses/Courses.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Course from "../Course/Course";
 
 
 const Courses = () => {
@@ -13,8 +14,13 @@ const Courses = () => {
     }, [])
 
     return (
-        <div>
+        <div className='md:w-2/3 border-2 my-5'> 
             <h2 className="text-xl">Total courses: {courses.length}</h2>
+            <div>
+                {
+                    courses.map(course => <Course key={course.id} course={course}/>)
+                }
+            </div>
         </div>
     );
 };
